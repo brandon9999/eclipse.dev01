767ac9f6c5a974d69f3ce209a4a42ed7986e8c2d
brandon9999 <induomo@gmail.com>
2018-11-24 11:46:02 +0900

    a

--- java.json/src/com/kis/ParseTest4.java
+++ java.json/src/com/kis/ParseTest4.java
@@ -34,48 +34,68 @@ public class ParseTest4 {
         	JsonObject jsonObject = (JsonObject) jsonParser.parse(new FileReader(
                     "D:\\nfs01\\DevHome\\Home.JSON\\curl01\\response.json"));
 
+           	////////////////////////////////////////////////////////////
+           	// json data : "jeusadmin-result"
+           	////////////////////////////////////////////////////////////
         	JsonObject dataObject = (JsonObject) jsonObject.get("jeusadmin-result");
-
           	System.out.print("message : " + dataObject.get("message"));
            	System.out.print("\n");
           	System.out.print("data : " + dataObject.get("data"));
            	System.out.print("\n");
+           	////////////////////////////////////////////////////////////
 
+           	System.out.print("\n");
            	
-        	JsonParser jsonParser2 = new JsonParser();
-           	Object obj2 = jsonParser2.parse(dataObject.get("data").toString());
-           	JsonArray jsonArray = (JsonArray) obj2;
-           	for(int i=0;i<jsonArray.size();i++)
+           	////////////////////////////////////////////////////////////
+           	// json data : "jeusadmin-result > data"
+           	////////////////////////////////////////////////////////////
+           	JsonParser jsonParser_data = new JsonParser();
+           	Object object_data = jsonParser_data.parse(dataObject.get("data").toString());
+           	JsonArray jsonArray_data = (JsonArray) object_data;
+
+       		JsonObject jsonObj_data = (JsonObject)jsonArray_data.get(0);
+       		System.out.print("title : " + jsonObj_data.get("title")+"\n");
+       		System.out.print("header : " +jsonObj_data.get("header")+"\n");
+       		System.out.print("column-names : " +jsonObj_data.get("column-names")+"\n");
+       		System.out.print("rows : " +jsonObj_data.get("rows")+"\n");
+           	////////////////////////////////////////////////////////////
+
+           	System.out.print("\n");
+
+           	////////////////////////////////////////////////////////////
+           	// json data : "jeusadmin-result > data > rows"
+           	////////////////////////////////////////////////////////////
+       		JsonParser jsonParse_data_rows = new JsonParser();
+           	Object ojbect_data_rows = jsonParse_data_rows.parse(jsonObj_data.get("rows").toString());
+           	JsonArray jsonArray_data_rows = (JsonArray) ojbect_data_rows;
+
+           	for(int i=0;i<jsonArray_data_rows.size();i++)
            	{
-           		JsonObject jsonObj = (JsonObject)jsonArray.get(i);
-           		System.out.print("title : " + jsonObj.get("title")+"\n");
-           		System.out.print("header : " +jsonObj.get("header")+"\n");
-           		System.out.print("column-names : " +jsonObj.get("column-names")+"\n");
-           		System.out.print("rows : " +jsonObj.get("rows")+"\n");
+	       		JsonObject jsonObj_data_rows = (JsonObject)jsonArray_data_rows.get(i);
+	       		System.out.print("row-key : " + jsonObj_data_rows.get("row-key")+"\n");
+	       		System.out.print("values : " + jsonObj_data_rows.get("values")+"\n");
+	       		
+	       		JsonParser jsonParse_tmp = new JsonParser();
+	           	Object ojbect_tmp = jsonParse_tmp.parse(jsonObj_data_rows.get("values").toString());
+	           	JsonArray jsonArray_tmp = (JsonArray) ojbect_tmp;
+	       		System.out.print("array size : " + jsonArray_tmp.size()+"\n");
+	       		
+	           	for(int k=0;k<jsonArray_tmp.size();k++)
+	           	{	       		
+		       		System.out.print("array data : " + k + " : " + jsonArray_tmp.get(k)+"\n");
 
-           		//System.out.print(jsonArray.get(i));
-       		}
-
-
-//           	
-//        	System.out.print("\n");
-//        	System.out.print("jsonArray size : " + jsonArray.size());
-//        	System.out.print("\n");
-//        	System.out.print("jsonArray toString: " + jsonArray.toString());
-            
-/*        	
-        	for(int i=0; i<jsonArray.size(); i++)
-        	{ 
-        		System.out.println("result :: " +jsonArray. get("title")); 
-        	}
-
-        	*/
-        	
-        	/*
-        	System.out.print("name : " + dataObject.get("name"));
-        	System.out.print("age : " + dataObject.get("age"));
-        	System.out.print("birth : " + dataObject.get("birth"));	
-*/
+	           	}
+	           	System.out.print("\n");
+           	}           	
+           	////////////////////////////////////////////////////////////
+           	
+           	
+   	
+           	
+           	
+           	
+           	
+           	
         } catch (Exception e) {
             e.printStackTrace();
         }
